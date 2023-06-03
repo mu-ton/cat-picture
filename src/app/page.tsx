06566b064c844290c9ad8574930b59550ad2c6da
yoshida884 <yoshida.1752@gmail.com>
2023-06-03 13:24:14 +0900

    add button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,38 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 export default function Home() {
-  const fetchImage = async () => {
+  const [imageUrl, setImageUrl] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  const fetchImage = async (): Promise<Image> => {
     const res = await fetch("https://api.thecatapi.com/v1/images/search");
     const images = await res.json();
     return images[0];
   };
 
+  const handleClick = async () => {
+    setLoading(true);
+    const newImage = await fetchImage();
+    setImageUrl(newImage.url);
+    setLoading(false);
+  };
+  useEffect(() => {
+    fetchImage().then((newImage) => {
+      setImageUrl(newImage.url);
+      setLoading(false);
+    });
+  }, []);
+
+  type Image = {
+    url: string;
+  };
+
   return (
     <div>
-      猫画像予定地
-      <h1>test</h1>
+      <button onClick={handleClick}>Click!</button>
+      {!loading && <img src={imageUrl} />}
     </div>
   );
 }
