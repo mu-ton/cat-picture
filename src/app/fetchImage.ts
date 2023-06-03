bed1a39427ba32a795442119b521ac014ad59c90
yoshida884 <yoshida.1752@gmail.com>
2023-06-03 19:29:09 +0900

    devide func

---
+++ src/app/fetchImage.ts
@@ -0,0 +1,11 @@
+type Image = {
+  url: string;
+};
+
+const fetchImage = async (): Promise<Image> => {
+  const res = await fetch("https://api.thecatapi.com/v1/images/search");
+  const images = await res.json();
+  return images[0];
+};
+
+export default fetchImage;
